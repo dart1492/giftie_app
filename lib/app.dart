2d5831fecf1506dddb1ab0172c3a33ee1b01d983
dart <dart78@ukr.net>
2023-03-28 21:32:22 +0300

    project rename

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:task_4/shared/app_colors.dart';
-import 'package:task_4/views/root/root.dart';
+import 'package:provider/provider.dart';
+import 'package:giftie_app/models/shopping_cart.dart';
+import 'package:giftie_app/shared/app_colors.dart';
+import 'package:giftie_app/views/root/root.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const Root(),
-      theme: ThemeData(
-        useMaterial3: true,
-        fontFamily: "Inter",
-        listTileTheme: ListTileThemeData(
-          tileColor: AppColors.lightWhiteGreen,
-          iconColor: AppColors.plainBlack,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5),
-            side: const BorderSide(color: AppColors.plainBlack),
+    return ChangeNotifierProvider(
+      create: (context) => ShoppingCart(),
+      child: MaterialApp(
+        home: const Root(),
+        theme: ThemeData(
+          useMaterial3: true,
+          fontFamily: "Inter",
+          listTileTheme: ListTileThemeData(
+            tileColor: AppColors.lightWhiteGreen,
+            iconColor: AppColors.plainBlack,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+              side: const BorderSide(color: AppColors.plainBlack),
+            ),
           ),
         ),
       ),
